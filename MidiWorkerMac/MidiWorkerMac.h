67d78d4b89439b84ca29c42c90c5b7c63091f2eb
Sinri Edogawa <e.joshua.s.e@gmail.com>
2020-05-12 15:56:21 +0800

    Initial Commit

---
+++ MidiWorkerMac/MidiWorkerMac.h
@@ -0,0 +1,19 @@
+//
+//  MidiWorkerMac.h
+//  MidiWorkerMac
+//
+//  Created by Sinri Edogawa on 2020/5/12.
+//  Copyright © 2020 Sinri Edogawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MidiWorkerMac.
+FOUNDATION_EXPORT double MidiWorkerMacVersionNumber;
+
+//! Project version string for MidiWorkerMac.
+FOUNDATION_EXPORT const unsigned char MidiWorkerMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MidiWorkerMac/PublicHeader.h>
+
+
